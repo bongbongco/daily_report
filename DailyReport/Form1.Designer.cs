a2c26ddb32471a7f859a8f20f658fb8c8918a5b0
Lee Seungyong <bongbongco@gmail.com>
2018-12-20 18:12:36 +0900

    기능 추가를 위한 UI 변경

--- DailyReport/Form1.Designer.cs
+++ DailyReport/Form1.Designer.cs
@@ -36,11 +36,25 @@
             this.label2 = new System.Windows.Forms.Label();
             this.startDateChange_Button = new System.Windows.Forms.Button();
             this.endDateChange_Button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // createButton
             // 
-            this.createButton.Location = new System.Drawing.Point(281, 95);
+            this.createButton.Location = new System.Drawing.Point(283, 101);
             this.createButton.Name = "createButton";
             this.createButton.Size = new System.Drawing.Size(326, 130);
             this.createButton.TabIndex = 0;
@@ -50,21 +64,21 @@
             // 
             // monthCalendar
             // 
-            this.monthCalendar.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar.Location = new System.Drawing.Point(20, 24);
             this.monthCalendar.Name = "monthCalendar";
             this.monthCalendar.TabIndex = 1;
             this.monthCalendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_DateChanged);
             // 
             // startDate_TextBox
             // 
-            this.startDate_TextBox.Location = new System.Drawing.Point(366, 15);
+            this.startDate_TextBox.Location = new System.Drawing.Point(368, 21);
             this.startDate_TextBox.Name = "startDate_TextBox";
             this.startDate_TextBox.Size = new System.Drawing.Size(159, 25);
             this.startDate_TextBox.TabIndex = 2;
             // 
             // endDate_TextBox
             // 
-            this.endDate_TextBox.Location = new System.Drawing.Point(366, 47);
+            this.endDate_TextBox.Location = new System.Drawing.Point(368, 53);
             this.endDate_TextBox.Name = "endDate_TextBox";
             this.endDate_TextBox.Size = new System.Drawing.Size(159, 25);
             this.endDate_TextBox.TabIndex = 3;
@@ -72,7 +86,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(278, 18);
+            this.label1.Location = new System.Drawing.Point(280, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 15);
             this.label1.TabIndex = 4;
@@ -81,7 +95,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(278, 50);
+            this.label2.Location = new System.Drawing.Point(280, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 15);
             this.label2.TabIndex = 5;
@@ -89,7 +103,7 @@
             // 
             // startDateChange_Button
             // 
-            this.startDateChange_Button.Location = new System.Drawing.Point(532, 15);
+            this.startDateChange_Button.Location = new System.Drawing.Point(534, 21);
             this.startDateChange_Button.Name = "startDateChange_Button";
             this.startDateChange_Button.Size = new System.Drawing.Size(75, 25);
             this.startDateChange_Button.TabIndex = 6;
@@ -99,7 +113,7 @@
             // 
             // endDateChange_Button
             // 
-            this.endDateChange_Button.Location = new System.Drawing.Point(532, 47);
+            this.endDateChange_Button.Location = new System.Drawing.Point(534, 53);
             this.endDateChange_Button.Name = "endDateChange_Button";
             this.endDateChange_Button.Size = new System.Drawing.Size(75, 25);
             this.endDateChange_Button.TabIndex = 7;
@@ -107,23 +121,141 @@
             this.endDateChange_Button.UseVisualStyleBackColor = true;
             this.endDateChange_Button.Click += new System.EventHandler(this.endDateChange_Button_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.endDateChange_Button);
+            this.groupBox1.Controls.Add(this.createButton);
+            this.groupBox1.Controls.Add(this.startDateChange_Button);
+            this.groupBox1.Controls.Add(this.monthCalendar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.startDate_TextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.endDate_TextBox);
+            this.groupBox1.Location = new System.Drawing.Point(18, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(632, 244);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "일일점검 결과 보고서 생성";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(678, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(399, 244);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "KIMS";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "출근";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "PW";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 102);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "퇴근";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "주간 모니터링";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(224, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 25);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "VPN 목록";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(18, 276);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1059, 214);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(224, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "작업 목록";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(616, 243);
-            this.Controls.Add(this.endDateChange_Button);
-            this.Controls.Add(this.startDateChange_Button);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.endDate_TextBox);
-            this.Controls.Add(this.startDate_TextBox);
-            this.Controls.Add(this.monthCalendar);
-            this.Controls.Add(this.createButton);
+            this.ClientSize = new System.Drawing.Size(1095, 502);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "ITS 부문 일일점검 결과 보고서 생성기";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -137,6 +269,18 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button startDateChange_Button;
         private System.Windows.Forms.Button endDateChange_Button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
